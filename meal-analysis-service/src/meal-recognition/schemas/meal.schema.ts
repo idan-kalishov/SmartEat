2ffb4dee9bf061c4c4f22db9ba1ee15c6aa75ffa
itamarattali <itamarattali@gmail.com>
2025-05-13 20:01:55 +0300

    feat(meal-recognition-backend) created a meal schema according to the final version of the ingredientDetails interface

---
+++ meal-analysis-service/src/meal-recognition/schemas/meal.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IngredientDetails } from '../types/ingredient-details.interface';
+
+@Schema({ timestamps: true })
+export class Meal extends Document {
+  @Prop({ required: true })
+  userId: string;
+
+  @Prop({ required: true, type: [Object] })
+  ingredients: IngredientDetails[];
+
+  @Prop()
+  imageUrl?: string;
+}
+
+export const MealSchema = SchemaFactory.createForClass(Meal);
