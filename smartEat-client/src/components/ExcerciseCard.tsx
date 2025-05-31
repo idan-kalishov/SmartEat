369501ab1404b21c842fdb380a8275b911d5442d
idan janach <idanjanach4455@gmail.com>
2025-05-31 18:14:35 +0300

    added excersise card to home page and fixed navbar position

---
+++ smartEat-client/src/components/ExcerciseCard.tsx
@@ -0,0 +1,24 @@
+import { Flame, Clock, Plus } from "lucide-react";
+
+const ExerciseCard = () => {
+  return (
+    <div className="bg-white rounded-xl shadow-sm p-4 m-2 w-[50%] align-right">
+      <div className="flex justify-between items-center mb-4">
+        <span className="text-black font-semibold text-lg">Exercise</span>
+        <Plus className="w-5 h-5 text-black" />
+      </div>
+
+      <div className="flex items-center gap-2 mb-2">
+        <Flame className="text-orange-500 w-5 h-5" />
+        <span className="text-gray-500 font-medium text-sm">0 cal</span>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Clock className="text-orange-500 w-5 h-5" />
+        <span className="text-gray-500 font-medium text-sm">00:00 hr</span>
+      </div>
+    </div>
+  );
+};
+
+export default ExerciseCard;
